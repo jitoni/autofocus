436ccbc0bd8bd0b78f9d91b40f1d6a41c861724d
jitoni <49103449+jitoni@users.noreply.github.com>
2020-06-14 01:47:58 +0200

    14.06.2020 (01:47)

--- adapter-luatojson/app.js
+++ adapter-luatojson/app.js
@@ -2,10 +2,13 @@
 const fs = require('fs')
 
 // ==== Main
-var file = 'Archrist.lua'
-var read = fs.readFileSync(file, 'utf-8')
+//:: Variables
+var readFile = 'example.lua'
+var writeFile = 'output.json'
 
-read = read
+//:: Manipulate readFile
+var cachedDocs = fs.readFileSync(readFile, 'utf-8')
+cachedDocs = cachedDocs
     .replace(/\/\*[\s\S]*?\*\/|([^:]|^)\-\-.*$/gm, '')                      //:: commentleri sil
     .replace(/  |\r\n|\n|\r|\t|\t\n/gm, "")                                 //:: remove tabs & returns
     .replace(/\=/g, ':')                                                    //:: = leri : ile degistir
@@ -16,11 +19,19 @@ read = read
     .replace(/^(.*?)(?<=\},"global"\s:)|^(.*?)(?<=\},\s"global"\s:)/, '')
     .replace(/\}$/, '')                                                     //:: sonraki tek } sil
     .replace(/,(\}|\])/g, '$1')                                             //:: remove trailing comma
-
-read = read
+cachedDocs = cachedDocs
     .replace(/\}\}/g,']]')
     .replace(/\]\]\]\]\,|\]\]\]\]/, ']]}}')
 
-// },"global" : 
+//:: Format the JSON  
+var obj = JSON.parse(cachedDocs)
+cachedDocs = JSON.stringify(obj, null, 4)
 
-fs.writeFileSync('output.json', read)
+//:: Write File
+fs.writeFile(writeFile, cachedDocs, function(err) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log("JSON saved to " + writeFile);
+    }
+})
