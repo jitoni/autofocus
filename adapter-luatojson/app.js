097cd637767c26f910666cceaf17d86ea3b80d6c
jitoni <49103449+jitoni@users.noreply.github.com>
2020-06-14 01:23:45 +0200

    14.06.2020 (01;23)

--- adapter-luatojson/app.js
+++ adapter-luatojson/app.js
@@ -3,22 +3,23 @@ const fs = require('fs')
 
 // ==== Main
 var file = 'Archrist.lua'
-var read = fs.readFileSync('Archrist.lua', 'utf-8')
+var read = fs.readFileSync(file, 'utf-8')
 
 read = read
-    .replace(/^(.*?)(?<=ArchLootDB\s\=\s)/, '')             //:: Variable declaration sil
-    .replace(/\=/g,':')                                     //:: = leri : ile degistir
-    .replace(/\/\*[\s\S]*?\*\/|([^:]|^)\-\-.*$/gm,'')       //:: commentleri sil
-    .replace(/  |\r\n|\n|\r/gm,"")                          //:: remove tabs & returns
-    // .replace(/--\s\[\d+\]/g,'')                          //:: numara commentlerini sil
-    .replace(/(\[(?="))|((?<=\")(\]))/g,'')                 //:: " den once ve sonraki [] karakterlerini sil
-    .replace(/\{\{/g,'[[')                                  //:: change double curlies to square brackets
-    .replace(/\}\,\{/g,'],[')                               //:: replace curly bracket set with square brackets
-    .replace(/\}\}\,/g,']],')
-    .replace(/\}\}\,/g,']],')
-    .replace(/\}\}\}/g,']]}')
-    .replace(/^(.*?)(?<=\},"global"\s:)/, '')  
-    .replace(/\}$/, '')             //:: Variable declaration sil
+    .replace(/\/\*[\s\S]*?\*\/|([^:]|^)\-\-.*$/gm, '')                      //:: commentleri sil
+    .replace(/  |\r\n|\n|\r|\t|\t\n/gm, "")                                 //:: remove tabs & returns
+    .replace(/\=/g, ':')                                                    //:: = leri : ile degistir
+    .replace(/^(.*?)(?<=ArchLootDB\s\:\s)/, '')                             //:: Variable declaration sil
+    .replace(/(\[(?="))|((?<=\")(\]))/g, '')                                //:: " den once ve sonraki [] karakterlerini sil
+    .replace(/\{\{/g, '[[')                                                 //:: change double curlies to square brackets
+    .replace(/\}\,\{/g, '],[')                                              //:: replace curly bracket set with square brackets
+    .replace(/^(.*?)(?<=\},"global"\s:)|^(.*?)(?<=\},\s"global"\s:)/, '')
+    .replace(/\}$/, '')                                                     //:: sonraki tek } sil
+    .replace(/,(\}|\])/g, '$1')                                             //:: remove trailing comma
+
+read = read
+    .replace(/\}\}/g,']]')
+    .replace(/\]\]\]\]\,|\]\]\]\]/, ']]}}')
 
 // },"global" : 
 
