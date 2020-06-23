176fb50d33305bd1c9eb6a24fc7fff455ef0e051
jitoni <49103449+jitoni@users.noreply.github.com>
2020-06-23 23:51:37 +0200

    23.06.2020 (uld10)

--- adapter-luatojson/app.js
+++ adapter-luatojson/app.js
@@ -3,8 +3,9 @@ const fs = require('fs')
 
 // ==== Main
 //:: Variables
-var readFile = '../../Archrist.lua'
+var readFile = 'Archrist.lua'
 var writeFile = 'output.json'
+var writeFile2 = 'lastRaid.json'
 
 //:: Manipulate readFile
 var cachedDocs = fs.readFileSync(readFile, 'utf-8')
@@ -24,10 +25,21 @@ cachedDocs = cachedDocs
     .replace(/\]\]\]\]\,|\]\]\]\]/, ']]}}')
     .replace(/\{\]\]\}/, '{}}}')
 
-//:: Format the JSON  
-var obj = JSON.parse(cachedDocs)
-cachedDocs = JSON.stringify(obj, null, 4)
+var cachedDocs2 = fs.readFileSync(readFile,'utf-8')
+cachedDocs2 = cachedDocs2
+    .replace(/\/\*[\s\S]*?\*\/|([^:]|^)\-\-.*$/gm, '')                        //:: commentleri sil
+    .replace(/  |\r\n|\n|\r|\t|\t\n/gm, "")                                   //:: remove tabs & returns
+    .replace(/\=/g, ':')                                                      //:: = leri : ile degistir
+    .replace(/(\[(?="))|((?<=\")(\]))/g, '')                                  //:: " den once ve sonraki [] karakterlerini si
+    .replace(/^(.*?)(?<="lastRaid"\s:)|^(.*?)(?<=\s"lastRaid"\s:)/, '') //:: start scope
+    .replace(/(?=\},).*$/, '}}')                                               //:: end scope
+    .replace(/,(\}|\])/g, '$1')                                               //:: remove trailing comma
+    .replace(/,\{/,':[')
+    .replace(/\}\}/,']}')
 
+//:: Format the JSON  
+var temp = JSON.parse(cachedDocs)
+cachedDocs = JSON.stringify(temp, null, 4)
 //:: Write File
 fs.writeFile(writeFile, cachedDocs, function(err) {
     if(err) {
@@ -35,4 +47,14 @@ fs.writeFile(writeFile, cachedDocs, function(err) {
     } else {
       console.log("JSON saved to " + writeFile);
     }
+})
+
+// temp = JSON.parse(cachedDocs2)
+// cachedDocs2 = JSON.stringify(temp, null, 4)
+fs.writeFile(writeFile2, cachedDocs2, function(err) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log("JSON saved to " + writeFile2);
+  }
 })
